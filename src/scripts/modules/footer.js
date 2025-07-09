aa47ea144adc5a1368832b01052262bf3d85ed45
ayden <150983776+cveryskys@users.noreply.github.com>
2025-07-09 10:06:12 -0500

    Update footer.js

--- src/scripts/modules/footer.js
+++ src/scripts/modules/footer.js
@@ -1,5 +1,8 @@
 export function renderFooter() {
   document.querySelector('.site-footer').innerHTML = `
-    <div style="color:#fff;text-align:center;animation:fadeInUp 1.1s;font-size:1.2rem;">:p</div>
-  `;
+<div style="color: #fff; text-align: center; animation: fadeInUp 1.1s; font-size: 1.2rem; font-weight: bold;">
+  If you're looking to hire me, DM me on the account I messaged you from — or the one you messaged me.  
+  If you didn’t get my username from Discord, try: <strong>pure_heroin</strong>
+</div>`
+;
 }
